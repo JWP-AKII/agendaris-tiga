3b8fdf6059fa1e365041fd2015ef5dff2d0ac750
agataartiko <144862999+agataartiko@users.noreply.github.com>
2023-09-13 13:29:08 +0800

    Add files via upload

---
+++ db_surat (1).sql
@@ -0,0 +1,226 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 13, 2023 at 07:28 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_surat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `id` int(11) NOT NULL,
+  `nama_buku` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`id`, `nama_buku`, `created_at`) VALUES
+(4, 'Komik Naruto', '2023-09-13 05:13:32'),
+(5, 'Komik One Piece', '2023-09-13 05:13:32'),
+(7, 'Buku Dasar-Dasar Teknik Informatika', '2023-09-13 05:15:26'),
+(8, 'Buku Pengantar Teknologi Informasi – Teknik Informatika', '2023-09-13 05:15:49'),
+(9, 'Metode Penelitian Teknik Informatika', '2023-09-13 05:16:03'),
+(10, 'Komputer Cerdas Untuk Mahasiswa Teknik Informatika', '2023-09-13 05:16:15'),
+(11, 'Teknik Pengolahan Audio & Video Kompetensi Keahlian Multimedia Program Keahlian Teknik Komputer dan Informatika', '2023-09-13 05:16:32'),
+(12, ' Buku Pengantar Teknologi Informatika Dan Komunikasi Data', '2023-09-13 05:16:45'),
+(13, ' Buku Aplikasi Komputer', '2023-09-13 05:17:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `disposisi_surat`
+--
+
+CREATE TABLE `disposisi_surat` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `surat_masuk_id` int(11) NOT NULL,
+  `disposisi` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `surat_keluar`
+--
+
+CREATE TABLE `surat_keluar` (
+  `id` int(11) NOT NULL,
+  `nomor_surat` int(255) NOT NULL,
+  `tanggal_surat` date NOT NULL DEFAULT current_timestamp(),
+  `tujuan` text NOT NULL,
+  `nomor_agenda` int(11) NOT NULL,
+  `tanggal_agenda` date NOT NULL DEFAULT current_timestamp(),
+  `buku_id` int(11) NOT NULL,
+  `status` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `surat_masuk`
+--
+
+CREATE TABLE `surat_masuk` (
+  `id` int(11) NOT NULL,
+  `nomor_surat` int(255) NOT NULL,
+  `tanggal_surat` date NOT NULL,
+  `pengirim` text NOT NULL,
+  `nomor_agenda` int(255) NOT NULL,
+  `tanggal_agenda` date NOT NULL,
+  `buku_id` int(11) NOT NULL,
+  `status` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `surat_masuk`
+--
+
+INSERT INTO `surat_masuk` (`id`, `nomor_surat`, `tanggal_surat`, `pengirim`, `nomor_agenda`, `tanggal_agenda`, `buku_id`, `status`) VALUES
+(1, 98765, '2023-09-01', 'BPVP Samarinda', 54321, '2023-09-05', 4, 1),
+(2, 56789, '2023-09-02', 'DPRD Samarinda', 789456, '2023-09-21', 5, 2),
+(3, 543101641, '2023-08-01', 'thf', 87642, '2023-09-30', 4, 2),
+(5, 123, '2023-09-02', 'Presiden', 321, '2023-09-22', 5, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `jabatan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `jabatan`) VALUES
+(1, 'budi', '123', 'HRD');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `disposisi_surat`
+--
+ALTER TABLE `disposisi_surat`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `surat_masuk_id` (`surat_masuk_id`);
+
+--
+-- Indexes for table `surat_keluar`
+--
+ALTER TABLE `surat_keluar`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `buku_id` (`buku_id`);
+
+--
+-- Indexes for table `surat_masuk`
+--
+ALTER TABLE `surat_masuk`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `buku_id` (`buku_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `disposisi_surat`
+--
+ALTER TABLE `disposisi_surat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `surat_keluar`
+--
+ALTER TABLE `surat_keluar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `surat_masuk`
+--
+ALTER TABLE `surat_masuk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `disposisi_surat`
+--
+ALTER TABLE `disposisi_surat`
+  ADD CONSTRAINT `disposisi_surat_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `disposisi_surat_ibfk_2` FOREIGN KEY (`surat_masuk_id`) REFERENCES `surat_masuk` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `surat_keluar`
+--
+ALTER TABLE `surat_keluar`
+  ADD CONSTRAINT `surat_keluar_ibfk_1` FOREIGN KEY (`buku_id`) REFERENCES `buku` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `surat_masuk`
+--
+ALTER TABLE `surat_masuk`
+  ADD CONSTRAINT `surat_masuk_ibfk_2` FOREIGN KEY (`buku_id`) REFERENCES `buku` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
